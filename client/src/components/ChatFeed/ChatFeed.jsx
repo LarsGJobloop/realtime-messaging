836d1b4f8596a13d8d11328d362b3dc16378d789
LarsG <lars.gunnar@jobloop.no>
2023-06-15 17:33:47 +0000

    formatting

--- client/src/components/ChatFeed/ChatFeed.jsx
+++ client/src/components/ChatFeed/ChatFeed.jsx
@@ -1,32 +1,25 @@
 import { ChatMessageCard } from "../ChatMessageCard/ChatMessageCard";
 import style from "./style.module.css";
 
-
 /**
- * 
+ *
  * @param {{
  *  feed: import("../../hooks/useChat/useChat").ChatMessage[]
- * }} props 
- * @returns 
+ * }} props
+ * @returns
  */
-export function ChatFeed({
-  feed
-}) {  
+export function ChatFeed({ feed }) {
   return (
     <section className={style["ChatFeed"]}>
       <h2>Message Feed</h2>
       <ul>
-        {
-          feed.map(
-            (chatMessage) => {
-              return (
-                <li key={chatMessage.id}>
-                  <ChatMessageCard message={chatMessage} />
-                </li>
-              )
-            }
-          )
-        }
+        {feed.map((chatMessage) => {
+          return (
+            <li key={chatMessage.id}>
+              <ChatMessageCard message={chatMessage} />
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
