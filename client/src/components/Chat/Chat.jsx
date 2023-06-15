12b7be8f36378101c4af4b04cbe94283f821d483
LarsG <lars.gunnar@jobloop.no>
2023-06-15 16:54:20 +0000

    setup consumption of mocks

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,12 +2,15 @@ import style from "./style.module.css";
 
 import { ChatFeed } from "../ChatFeed/ChatFeed";
 import { ChatInput } from "../ChatInput/ChatInput";
+import { useChat } from "../../hooks/useChat/useChat";
 
 export function Chat() {
+  const { messages, postMessage } = useChat("commonRoom");
+
   return (
     <div className={style["Chat"]}>
-      <ChatFeed />
-      <ChatInput />
+      <ChatFeed feed={messages} />
+      <ChatInput onPostMessage={postMessage} />
     </div>
   );
 }
--- client/src/components/ChatFeed/ChatFeed.jsx
+++ client/src/components/ChatFeed/ChatFeed.jsx
@@ -1,9 +1,40 @@
 import style from "./style.module.css";
 
-export function ChatFeed() {
+
+/**
+ * 
+ * @param {{
+ *  feed: import("../../hooks/useChat/useChat").ChatMessage[]
+ * }} props 
+ * @returns 
+ */
+export function ChatFeed({
+  feed
+}) {  
   return (
     <section className={style["ChatFeed"]}>
       <h2>Message Feed</h2>
+      <ul>
+        {
+          feed.map(
+            (chatMessage) => {
+              return (
+                <li key={chatMessage.id}>
+                  <div>
+                    <div>
+                      <span>{chatMessage.author.alias}</span>
+                      <span>{chatMessage.createdAt}</span>
+                    </div>
+                    <p>
+                      {chatMessage.body}
+                    </p>
+                  </div>
+                </li>
+              )
+            }
+          )
+        }
+      </ul>
     </section>
   );
 }
