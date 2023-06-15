d84082fa2f1d9f9a3e6936ab700590e5c683de12
LarsG <lars.gunnar@jobloop.no>
2023-06-15 19:31:13 +0000

    move mock messages to seperate file

--- client/src/hooks/useChat/useChat.js
+++ client/src/hooks/useChat/useChat.js
@@ -1,47 +1,4 @@
-/**
- * @typedef {{
- *  alias: string
- * }} User
- */
-
-/**
- * @typedef {{
- *  id: UniqueID
- *  author: User
- *  body: string
- *  createdAt: DateTime
- * }} ChatMessage
- */
-
-/**
- * @type {ChatMessage[]}
- */
-const mockMessages = [
-  {
-    id: "hello",
-    author: {alias: "Superman"},
-    body: "Hello World",
-    createdAt: Date.now() + 3124324,
-  },
-  {
-    id: "World",
-    author: {alias: "Superman"},
-    body: "Hello World",
-    createdAt: Date.now() + 4324324,
-  },
-  {
-    id: "Batman",
-    author: {alias: "Superman"},
-    body: "Hello World",
-    createdAt: Date.now() + 5424324,
-  },
-  {
-    id: "Fizz",
-    author: {alias: "Superman"},
-    body: "Hello World",
-    createdAt: Date.now() + 764324,
-  }
-]
+import { mockMessages } from "./mockMessages"
 
 
 export function useChat(room) {
