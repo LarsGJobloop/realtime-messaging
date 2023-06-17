6cb72d99324712888752720c796e743d89299ecc
LarsG <lars.gunnar@jobloop.no>
2023-06-17 15:55:28 +0000

    organize files

--- client/src/hooks/useChat/useChat.js
+++ client/src/hooks/useChat/useChat.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import messageBrooker from "../../utils/messageBrooker";
+import {connect as connectToMessageBrooker} from "../../utils/messageBrooker"
 
 // Our types
 /**
@@ -41,7 +41,7 @@ export function useChat({ room, alias }) {
   useEffect(() => {
     if (isConnected || error !== null) return;
 
-    const { disconnect, connectionStatus } = simplifiedConnect({
+    const { disconnect, connectionStatus } = connectToMessageBrooker({
       errorHandler: setError,
       onNewMessage: handleNewMessage,
       sendMessage,
@@ -81,82 +81,5 @@ export function useChat({ room, alias }) {
     messages,
     postMessage,
     error,
-  };
-}
-
-import { StringCodec } from "nats.ws";
-
-function decodeMessage(message) {
-  const codec = StringCodec();
-  return codec.decode(message);
-}
-
-/**
- * @typedef {{
- *  onNewMessage: (message: string) => void
- *  sendMessage: (message: string) => void
- *  errorHandler: (message: string, error, any) => void
- *  roomMeta: RoomMetaInformation
- * }} ChatRoomConnectionOptions
- */
-
-/**
- * @typedef {{
- *    name: string
- *    userAlias: string
- *  }} RoomMetaInformation
- */
-
-/**
- * Handles connecting to a message brooker and
- * exposes a selction of functionality
- *
- * @param {ChatRoomConnectionOptions} options
- *
- * @returns {{
- *  disconnect: () => void
- *  error: any
- *  connectionStatus: boolean
- * }}
- */
-function simplifiedConnect(options) {
-  const { errorHandler, onNewMessage, sendMessage, roomMeta } = options;
-
-  let this_connection;
-
-  function messageCallback(error, message) {
-    if (error !== null) {
-      console.error("??? NATS error", error);
-    }
-
-    const decodedMessage = decodeMessage(message.data);
-    const parsedMessage = JSON.parse(decodedMessage);
-
-    onNewMessage(error, parsedMessage);
-  }
-
-  messageBrooker.connect(
-    (connection) => {
-      this_connection = connection;
-      this_connection.subscribe(">", {
-        callback: messageCallback,
-      });
-
-      sendMessage((message) => {
-        const newMessage = messageBrooker.formatMessage(
-          message,
-          roomMeta.userAlias
-        );
-        this_connection.publish(roomMeta.name, newMessage);
-      });
-    },
-    (error) => errorHandler(error)
-  );
-
-  return {
-    disconnect: () => {
-      this_connection.close().catch((error) => console.error(error));
-    },
-    connectionStatus: this_connection ? true : false,
   };
 }
---
+++ client/src/utils/messageBrooker/natsBrooker/index.js
@@ -0,0 +1,77 @@
+import { connectToNatsServer } from "./connectToNatsServer"
+import { formatMessage } from "./formatMessage"
+
+import { StringCodec } from "nats.ws";
+
+function decodeMessage(message) {
+  const codec = StringCodec();
+  return codec.decode(message);
+}
+/**
+ * @typedef {{
+ *  onNewMessage: (message: string) => void
+ *  sendMessage: (message: string) => void
+ *  errorHandler: (message: string, error, any) => void
+ *  roomMeta: RoomMetaInformation
+ * }} ChatRoomConnectionOptions
+ */
+/**
+ * @typedef {{
+ *    name: string
+ *    NatsConnection,
+ *    userAlias: string
+ *  }} RoomMetaInformation
+ */
+/**
+ * Handles connecting to a message brooker and
+ * exposes a selction of functionality
+ *
+ * @param {ChatRoomConnectionOptions} options
+ *
+ * @returns {{
+ *  disconnect: () => void
+ *  error: any
+ *  connectionStatus: boolean
+ * }}
+ */
+export function connect(options) {
+  const { errorHandler, onNewMessage, sendMessage, roomMeta } = options;
+
+  let this_connection;
+
+  function messageCallback(error, message) {
+    if (error !== null) {
+      console.error("??? NATS error", error);
+    }
+
+    const decodedMessage = decodeMessage(message.data);
+    const parsedMessage = JSON.parse(decodedMessage);
+
+    onNewMessage(error, parsedMessage);
+  }
+
+  connectToNatsServer(
+    (connection) => {
+      this_connection = connection;
+      this_connection.subscribe(">", {
+        callback: messageCallback,
+      });
+
+      sendMessage((message) => {
+        const newMessage = formatMessage(
+          message,
+          roomMeta.userAlias
+        );
+        this_connection.publish(roomMeta.name, newMessage);
+      });
+    },
+    (error) => errorHandler(error)
+  );
+
+  return {
+    disconnect: () => {
+      this_connection.close().catch((error) => console.error(error));
+    },
+    connectionStatus: this_connection ? true : false,
+  };
+}
