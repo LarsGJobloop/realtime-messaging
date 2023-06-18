16b823193ef111bcda0a87d9ffc40258cc84227b
LarsG <lars.gunnar@jobloop.no>
2023-06-18 14:40:08 +0000

    component: Admin

---
+++ client/ts-variant/src/components/ToAdmin/ToAdmin.jsx
@@ -0,0 +1,16 @@
+import style from './style.module.css'
+import {useLocation} from "react-router"
+
+export function ToAdmin() {
+  const location = useLocation()
+
+  // Event Handlers
+  function navigateToAdminPage() {
+    console.log("Navigating to Admin Page from: ")
+    console.log(location)
+  }
+
+  return (
+    <button onClick={navigateToAdminPage} className={style['container']}>Admin</button>
+  )
+}
