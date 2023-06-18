9b414e6f66ef208c2f20854799cbf55f1f1cbf3f
LarsG <lars.gunnar@jobloop.no>
2023-06-18 14:18:50 +0000

    import global css files

--- client/ts-variant/src/main.tsx
+++ client/ts-variant/src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+// Global CSS
+import './styles/normalize.css'
+import './styles/palettes.css'
+import './styles/typography.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
