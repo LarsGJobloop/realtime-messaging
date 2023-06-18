401b1c5f2fa3f4e9ed17ef62a671f304ee15c3f5
LarsG <lars.gunnar@jobloop.no>
2023-06-18 20:56:52 +0000

    set server default to dev mode

--- backend/start-server.sh
+++ backend/start-server.sh
@@ -2,4 +2,4 @@
 
 CONFIG="${PWD}/config/nats-server.conf"
 
-docker run --name nats-server --rm -v $CONFIG:/nats-server.conf nats -c /nats-server.conf
+docker run --name nats-server --rm -v $CONFIG:/nats-server.conf nats -c /nats-server.conf -D
